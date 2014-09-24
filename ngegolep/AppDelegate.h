8c27a3a5a52d2e0130e4a1f66011c52c0796bd81
Rukmono Erwan <rukmono.erwan@gmail.com>
2014-09-24 14:28:01 +0700

    Initial Commit

---
+++ ngegolep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ngegolep
+//
+//  Created by Rukmono Erwan on 9/24/14.
+//  Copyright (c) 2014 palawamaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ngegolep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ngegolep
+//
+//  Created by Rukmono Erwan on 9/24/14.
+//  Copyright (c) 2014 palawamaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
